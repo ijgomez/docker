1f5a250f4e6a54d3fc1b1808095e3d7f7d52696b
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2020-03-06 20:40:21 +0100

    upload

--- hadoop/master/start-master.sh
+++ hadoop/master/start-master.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo "Run Master...."
+
+#Start Services
+hadoop-daemon.sh --script hdfs start namenode
+#yarn-daemon.sh start resourcemanager
+
+#Stop Services
+#yarn-daemon.sh stop resourcemanager
+#hadoop-daemon.sh --script hdfs stop namenode
--- hadoop/slave/start-slave.sh
+++ hadoop/slave/start-slave.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo "Run Slave...."
+
+#Start Services
+#hadoop-daemon.sh --script hdfs start datanode
+#yarn-daemon.sh start nodemanager
+
+#Stop Services
+#yarn-daemon.sh stop nodemanager
+#hadoop-daemon.sh --script hdfs stop datanode
