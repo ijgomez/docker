65a495a8a2f57c8b750d40e7797f3a24670d41d4
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2020-05-02 19:34:39 +0200

    update files

--- weblogic/container.run.sh
+++ weblogic/container.run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run -d --name oracle-wl -p 7001:7001 -p 9002:9002 -v /home/mdc/weblogic/domain.properties:/u01/oracle/properties/domain.properties store/oracle/weblogic:12.2.1.3-dev
-
+docker run -d --name oracle-wl -p 7001:7001 -p 9002:9002 -v ${PWD}:/u01/oracle/properties container-registry.oracle.com/middleware/weblogic:12.2.1.3
